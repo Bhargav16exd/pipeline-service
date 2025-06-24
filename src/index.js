c0eff4726f3ef03113bc469b3d5a7f21c091b42c
Bhargav Mule <bhargavmule16@gmail.com>
2025-06-24 20:47:16 +0530

    cors fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  credentials:true,
+  origin:process.env.ORIGIN_URL
+}))
 
 const oauth2Client = new google.auth.OAuth2(
     process.env.CLIENT_ID,
